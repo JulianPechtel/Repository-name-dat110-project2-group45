7311c55a88f352ee1ea04a696c52449a7fdbfc9e
JulianPechtel <julianpechtel@gmail.com>
2026-03-11 13:06:02 +0100

    start
    s

--- src/main/java/no/hvl/dat110/broker/Storage.java
+++ src/main/java/no/hvl/dat110/broker/Storage.java
@@ -4,19 +4,15 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import no.hvl.dat110.common.TODO;
 import no.hvl.dat110.common.Logger;
 import no.hvl.dat110.messagetransport.Connection;
 
 public class Storage {
 
-	// data structure for managing subscriptions
 	// maps from a topic to set of subscribed users
 	protected ConcurrentHashMap<String, Set<String>> subscriptions;
-	
-	// data structure for managing currently connected clients
+
 	// maps from user to corresponding client session object
-	
 	protected ConcurrentHashMap<String, ClientSession> clients;
 
 	public Storage() {
@@ -29,73 +25,77 @@ public class Storage {
 	}
 
 	public Set<String> getTopics() {
-
 		return subscriptions.keySet();
-
 	}
 
-	// get the session object for a given user
-	// session object can be used to send a message to the user
-	
 	public ClientSession getSession(String user) {
-
-		ClientSession session = clients.get(user);
-
-		return session;
+		return clients.get(user);
 	}
 
 	public Set<String> getSubscribers(String topic) {
-
-		return (subscriptions.get(topic));
-
+		return subscriptions.get(topic);
 	}
 
 	public void addClientSession(String user, Connection connection) {
 
-		// TODO: add corresponding client session to the storage
-		// See ClientSession class
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		// create and register a new session for the user
+		ClientSession session = new ClientSession(user, connection);
+		clients.put(user, session);
+
+		Logger.log("Storage: added client session for " + user);
 	}
 
 	public void removeClientSession(String user) {
 
-		// TODO: disconnet the client (user) 
-		// and remove client session for user from the storage
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		// remove session from storage
+		ClientSession session = clients.remove(user);
+
+		// disconnect if it existed
+		if (session != null) {
+			session.disconnect();
+		}
+
+		// also remove user from all topic subscription sets (cleanup)
+		for (String topic : subscriptions.keySet()) {
+			Set<String> subs = subscriptions.get(topic);
+			if (subs != null) {
+				subs.remove(user);
+			}
+		}
+
+		Logger.log("Storage: removed client session for " + user);
 	}
 
 	public void createTopic(String topic) {
 
-		// TODO: create topic in the storage
+		// create topic if it does not exist
+		subscriptions.putIfAbsent(topic, ConcurrentHashMap.newKeySet());
 
-		throw new UnsupportedOperationException(TODO.method());
-	
+		Logger.log("Storage: created topic " + topic);
 	}
 
 	public void deleteTopic(String topic) {
 
-		// TODO: delete topic from the storage
+		subscriptions.remove(topic);
 
-		throw new UnsupportedOperationException(TODO.method());
-		
+		Logger.log("Storage: deleted topic " + topic);
 	}
 
 	public void addSubscriber(String user, String topic) {
 
-		// TODO: add the user as subscriber to the topic
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		// ensure topic exists, then add user to subscriber set
+		subscriptions.computeIfAbsent(topic, t -> ConcurrentHashMap.newKeySet()).add(user);
+
+		Logger.log("Storage: " + user + " subscribed to " + topic);
 	}
 
 	public void removeSubscriber(String user, String topic) {
 
-		// TODO: remove the user as subscriber to the topic
+		Set<String> subs = subscriptions.get(topic);
+		if (subs != null) {
+			subs.remove(user);
+		}
 
-		throw new UnsupportedOperationException(TODO.method());
+		Logger.log("Storage: " + user + " unsubscribed from " + topic);
 	}
 }
--- src/main/java/no/hvl/dat110/messagetransport/MessagingServer.java
+++ src/main/java/no/hvl/dat110/messagetransport/MessagingServer.java
@@ -1,63 +1,61 @@
 package no.hvl.dat110.messagetransport;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class MessagingServer {
 
 	private ServerSocket welcomeSocket;
-	
+
 	public MessagingServer(int port) {
-		
+
 		try {
-		
-			this.welcomeSocket = new ServerSocket(port);
-			
+			// Create server socket with reuse enabled (important for repeated tests)
+			this.welcomeSocket = new ServerSocket();
+			this.welcomeSocket.setReuseAddress(true);
+			this.welcomeSocket.bind(new InetSocketAddress(port));
+
 		} catch (IOException ex) {
-			
 			System.out.println("Messaging server: " + ex.getMessage());
 			ex.printStackTrace();
+			this.welcomeSocket = null; // make sure it's null if setup failed
 		}
 	}
 
 	// accept an incoming connection from a client
-	public Connection accept () {
-		
-		Connection connection = null;
-		
-		// TODO
-		// accept TCP connection on welcome socket and create connection
-		
+	public Connection accept() {
+
+		// if server socket was not created (e.g., port already in use)
+		if (welcomeSocket == null || welcomeSocket.isClosed()) {
+			return null;
+		}
+	
 		try {
-			
 			Socket connectionSocket = welcomeSocket.accept();
-			
-			connection = new Connection(connectionSocket);
-			
+			return new Connection(connectionSocket);
 		} catch (IOException ex) {
-			
 			System.out.println("Messaging server: " + ex.getMessage());
 			ex.printStackTrace();
-			// TODO: closing welcomeSocket
+			stop();               // close welcomeSocket safely
+			return null;
 		}
-		
-		return connection;
-
 	}
 	
+
 	public void stop() {
-		
+
 		if (welcomeSocket != null) {
-			
 			try {
-			welcomeSocket.close();
+				welcomeSocket.close();
 			} catch (IOException ex) {
-				
-			System.out.println("Messaging server: " + ex.getMessage());
-			ex.printStackTrace();
-		}
+				System.out.println("Messaging server: " + ex.getMessage());
+				ex.printStackTrace();
+			} finally {
+				welcomeSocket = null;
+			}
 		}
 	}
-
 }
